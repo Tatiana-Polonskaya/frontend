844100500eb89297e9efd7b0781b15e09a5efa7a
user <alex.thunder@tut.by>
2023-04-27 18:22:02 +0300

    wip: add password restore css

--- src/pages/EntryPage/PasswordRestorePage/index.tsx
+++ src/pages/EntryPage/PasswordRestorePage/index.tsx
@@ -7,16 +7,36 @@ import { useNavigate } from "react-router-dom";
 import TextInput from "../../../components/ui-kit/TextInput";
 import Button from "../../../components/ui-kit/Button";
 
+import "./style.scss";
+import { cn } from "@bem-react/classname";
+
+const cnPasswordRestorePage = cn("password-restore-page");
+
 export default function PasswordRestorePage() {
     const navigate = useNavigate();
 
     return (
         <EntryLayout image={<ReactSVG src={PasswordRestoreImage} />}>
-            <Link onClick={() => navigate(-1)}>Вернуться</Link>
-            <p>Восстановление пароля</p>
-            <p>Введите почту, указанную при регистрации. Туда мы вышлем новый пароль. Вы всегда можете изменить его в настройках.</p>
-            <TextInput label="Почта" placeholder="Укажите почту" />
-            <Button>Продолжить</Button>
+            <div className={cnPasswordRestorePage()}>
+                <Link
+                    onClick={() => navigate(-1)}
+                    arrow="left"
+                    className={cnPasswordRestorePage("return-link")}
+                >
+                    Вернуться
+                </Link>
+                <p className={cnPasswordRestorePage("title")}>
+                    Восстановление пароля
+                </p>
+                <p className={cnPasswordRestorePage("description")}>
+                    Введите почту, указанную при регистрации. Туда мы вышлем
+                    новый пароль. Вы всегда можете изменить его в настройках.
+                </p>
+                <TextInput label="Почта" placeholder="Укажите почту" />
+                <Button className={cnPasswordRestorePage("next-button")}>
+                    Продолжить
+                </Button>
+            </div>
         </EntryLayout>
     );
 }
