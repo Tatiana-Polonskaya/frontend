c6dbe9c9c5385c8bc15420e5db06f73a0f47c277
Alexey Lagun <alexey.lagun@gmail.com>
2023-06-23 01:26:04 +0300

    feat: add component video progress panel

--- src/pages/DiaryPage/index.tsx
+++ src/pages/DiaryPage/index.tsx
@@ -2,24 +2,30 @@ import { Fragment } from "react";
 import MainLayout from "../../layouts/MainLayout";
 import { IVideoFromBack } from "../../models/video";
 
-import { useGetVideoByIdQuery, useGetVideoByUserQuery } from "../../store/api/userVideo";
-import ReactPlayer from 'react-player';
-
+import {
+    useGetVideoByIdQuery,
+    useGetVideoByUserQuery,
+} from "../../store/api/userVideo";
+import ReactPlayer from "react-player";
+import VideoProgressPanel from "../../components/VideoProgressPanel";
 
 export default function DiaryPage() {
-    
     const { data } = useGetVideoByUserQuery();
     const userVideos = data?.data?.videos as IVideoFromBack[];
-    console.log(userVideos);
+    // console.log(userVideos);
 
     return (
         <MainLayout>
             {userVideos &&
                 userVideos.map((el) => (
                     <Fragment key={el.id}>
-                        <ReactPlayer url={`api/video/${el.id}`}/>
+                        <ReactPlayer url={`api/video/${el.id}`} />
                     </Fragment>
                 ))}
+            <VideoProgressPanel
+                result={[10, 20, 30, 40, 50, 60]}
+                type={"small"}
+            />
         </MainLayout>
     );
 }
