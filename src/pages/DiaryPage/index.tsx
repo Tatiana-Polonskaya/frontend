629d4ca151709840a0c13d7c44a4924bd40f152e
Tatiana <tanya18082000@mail.ru>
2023-06-22 17:15:53 +0300

    fix: dairy page

--- src/pages/DiaryPage/index.tsx
+++ src/pages/DiaryPage/index.tsx
@@ -5,19 +5,15 @@ import { IVideoFromBack } from "../../models/video";
 import { useGetVideoByIdQuery, useGetVideoByUserQuery } from "../../store/api/userVideo";
 import ReactPlayer from 'react-player';
 
-const idVideo = "95eddb64-6656-4cd8-8c88-6fc680d8fecd";
 
 export default function DiaryPage() {
     
     const { data } = useGetVideoByUserQuery();
     const userVideos = data?.data?.videos as IVideoFromBack[];
     console.log(userVideos);
-    const videoData  = useGetVideoByIdQuery("feb81d20-2bb0-4622-b41a-3c6d50c6b3f8");
 
     return (
         <MainLayout>
-            <ReactPlayer url='https://www.youtube.com/watch?v=RKozwlUeCiQ'/>
-            <ReactPlayer url={`http://dev.speech-up.online/api/video/${idVideo}.mp4`} />
             {userVideos &&
                 userVideos.map((el) => (
                     <Fragment key={el.id}>
