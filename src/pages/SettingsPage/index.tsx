82655fa04f02fea8b2fc1dadd40802d24e22a191
AnastasiaKlets <klecz.anastasia@gmail.com>
2023-06-30 18:17:07 +0300

    add

--- src/pages/SettingsPage/index.tsx
+++ src/pages/SettingsPage/index.tsx
@@ -1,9 +1,13 @@
 import MainLayout from "../../layouts/MainLayout";
+import BadGoodBlock from "../../components/BadGoodBlock";
+import BlockGeneralAnalytics from "../../components/BlockGeneralAnalytics";
 
 export default function SettingsPage() {
     return (
         <MainLayout>
             <div>SettingsPage</div>
+            <BadGoodBlock/>
+            <BlockGeneralAnalytics N={9}  rank={"sdvsd"}/>
         </MainLayout>
     );
 }
