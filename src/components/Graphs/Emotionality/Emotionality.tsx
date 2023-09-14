440a6938943cfced6c659a21d7e0c70c2037228c
Tatiana <tanya18082000@mail.ru>
2023-09-14 15:03:02 +0300

    rename smile

--- src/components/Graphs/Emotionality/Emotionality.tsx
+++ src/components/Graphs/Emotionality/Emotionality.tsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import './style.css';
-import {ISimel} from "../../../models/graph/inteface/ISimel";
-import Smiley from '../smile/index'
-import okImg from './img/ok.svg'
-import happyImg from './img/happy.svg'
-import angryImg from './img/angry.svg'
+import React from "react";
+import "./style.css";
+import { ISimel } from "../../../models/graph/inteface/ISimel";
+import Smiley from "../SmileBlock/index";
+import okImg from "./img/ok.svg";
+import happyImg from "./img/happy.svg";
+import angryImg from "./img/angry.svg";
 
-type Props={
+type Props = {
     total: {
-        neutral: number,
-        happiness: number,
-        anger: number
-    }
-}
+        neutral: number;
+        happiness: number;
+        anger: number;
+    };
+};
 
 function Emotionality(props: Props) {
-    let inf: ISimel ={
-        items:
-        [
+    let inf: ISimel = {
+        items: [
             {
-                id:1,
-                title:"Злость",
-                procent:props.total.anger,
+                id: 1,
+                title: "Злость",
+                procent: props.total.anger,
                 colorProcent: "#FF4E78",
-                icon: angryImg
+                icon: angryImg,
             },
             {
-                id:2,
-                title:"Без эмоций",
-                procent:props.total.neutral,
+                id: 2,
+                title: "Без эмоций",
+                procent: props.total.neutral,
                 colorProcent: "#7C8EB5",
-                icon: okImg
+                icon: okImg,
             },
             {
-                id:3,
-                title:"Радость",
-                procent:props.total.happiness,
+                id: 3,
+                title: "Радость",
+                procent: props.total.happiness,
                 colorProcent: "#10CE7E",
-                icon: happyImg
-            }
-        ]
-    }
+                icon: happyImg,
+            },
+        ],
+    };
 
     return (
         <>
-            <div className='allEmotionality'>
+            <div className="allEmotionality">
                 <div className="imgAllEmotionality">
-                    <Smiley component={inf}/>
+                    <Smiley component={inf} />
                 </div>
             </div>
         </>
-    );}
+    );
+}
 
 export default Emotionality;
--- src/components/Graphs/communicativeNorm/index.tsx
+++ src/components/Graphs/communicativeNorm/index.tsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import {ISimel} from "../../../models/graph/inteface/ISimel";
+import React from "react";
+import { ISimel } from "../../../models/graph/inteface/ISimel";
 import angryImg from "./img/angry.svg";
-import cognitiveDistortionsImg from'./img/CognitiveDistortions.svg';
-import parasiteWordsImg from './img/ParasiteWords.svg';
-import Smiley from "../smile";
-import './style.scss'
+import cognitiveDistortionsImg from "./img/CognitiveDistortions.svg";
+import parasiteWordsImg from "./img/ParasiteWords.svg";
+import Smiley from "../SmileBlock";
+import "./style.scss";
 
-type Props={
-    filler_words: number,
-    cognitive_distortion: number,
-    aggression: number
-}
+type Props = {
+    filler_words: number;
+    cognitive_distortion: number;
+    aggression: number;
+};
 
-function CommunicativeNorm( props: Props) {
-   let inf: ISimel = {
-        items:
-            [
-                {
-                    id: 1,
-                    title: "Слова-паразиты",
-                    procent: props.filler_words,
-                    colorProcent: "#2477F4",
-                    icon: parasiteWordsImg
-                },
-                {
-                    id: 2,
-                    title: "Когнитивныe искажения",
-                    procent:props.cognitive_distortion,
-                    colorProcent: "#FFB800",
-                    icon: cognitiveDistortionsImg
-                },
-                {
-                    id: 3,
-                    title: "Агрессивность",
-                    procent:props.aggression,
-                    colorProcent: "#E23339",
-                    icon: angryImg
-                }
-            ]
-    }
+function CommunicativeNorm(props: Props) {
+    let inf: ISimel = {
+        items: [
+            {
+                id: 1,
+                title: "Слова-паразиты",
+                procent: props.filler_words,
+                colorProcent: "#2477F4",
+                icon: parasiteWordsImg,
+            },
+            {
+                id: 2,
+                title: "Когнитивныe искажения",
+                procent: props.cognitive_distortion,
+                colorProcent: "#FFB800",
+                icon: cognitiveDistortionsImg,
+            },
+            {
+                id: 3,
+                title: "Агрессивность",
+                procent: props.aggression,
+                colorProcent: "#E23339",
+                icon: angryImg,
+            },
+        ],
+    };
     return (
         <>
             <div className="CommunicativeNormBloc">
-                <Smiley component={inf}/>
+                <Smiley component={inf} />
             </div>
         </>
-    );}
+    );
+}
 export default CommunicativeNorm;
