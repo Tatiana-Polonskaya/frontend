0b993efd481cb8855b0b35a46a1c32f6d17040d2
Tatiana <tanya18082000@mail.ru>
2023-10-20 10:35:31 +0300

    fix: correction to the truth

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,27 @@
 import "./style.scss";
 
-import LogoTitle from "./assets/logo-title.svg";
-
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <img src={LogoTitle} alt="Logo" />
-                <div className="footer-content__links-container">
-                    <a
-                        className="footer-content__link"
-                        href="http://speechup.ru/confidential/"
-                    >
-                        {"Политика конфиденциальности"}
-                    </a>
-                    <a
-                        className="footer-content__link"
-                        href="http://speechup.ru/usersconf/"
-                    >
-                        {"Пользовательское соглашение"}
-                    </a>
-                </div>
-                <span>&#169;&nbsp;ООО&nbsp;«Спичап»&nbsp;2023</span>
+                <span>
+                    Все авторские права принадлежат создателям.
+                    <br /> Разработчики:
+                    <a href="https://github.com/4in4in">Леонов Александр</a>
+                    {", "}
+                    <a href="https://github.com/4in4in">Полонская Татьяна</a>
+                    {", "}
+                    <a href="https://github.com/4in4in">Лагун Алексей</a>
+                    {", "}
+                    <a href="https://github.com/4in4in">Ванькович Анастасия</a>
+                    <br />
+                    Дизайнер:
+                    <a href="https://t.me/ann_choys">Эгамова Анна</a>
+                </span>
+                <span>
+                    ©️ Leonov A., Polonskaia T., Lagun A., Vancovich A., Egamova
+                    A.
+                </span>
             </div>
         </footer>
     );
